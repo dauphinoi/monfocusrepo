2eef3bf82ca97023c91572ab7b356ac0950ebfd8
dauphinoi <ibrahima.diao@dauphine.eu>
2024-08-19 20:31:57 +0200

    institution test2

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+pip install -U django-storages
+pip install -U boto3
 python nltk_download.py
 python manage.py collectstatic --no-input
 python manage.py migrate
