48b069f11356a498d379d2034237e6c371f922de
dauphinoi <ibrahima.diao@dauphine.eu>
2024-08-20 15:42:52 +0200

    institution test14

--- monFocus/storage_backends.py
+++ monFocus/storage_backends.py
@@ -3,9 +3,18 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.AWS_STATIC_LOCATION
-    default_acl = 'public-read'
+    default_acl = None  # Désactive l'utilisation des ACLs
 
 class MediaStorage(S3Boto3Storage):
     location = settings.AWS_MEDIA_LOCATION
-    default_acl = 'private'
+    default_acl = None  # Désactive l'utilisation des ACLs
     file_overwrite = False
+
+    def get_accessed_time(self, name):
+        return None
+
+    def get_created_time(self, name):
+        return None
+
+    def path(self, name):
+        return None
