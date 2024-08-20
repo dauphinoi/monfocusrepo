4dcc84c3640c98632e3118b979fa0b30ef5c8071
dauphinoi <ibrahima.diao@dauphine.eu>
2024-08-21 00:41:15 +0200

    institution test19

--- monFocus/storage_backends.py
+++ monFocus/storage_backends.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 from urllib.parse import urljoin
+from django.core.exceptions import SuspiciousOperation
+import os
 
 class StaticStorage(S3Boto3Storage):
     location = settings.AWS_STATIC_LOCATION
@@ -13,6 +15,27 @@ class MediaStorage(S3Boto3Storage):
 
     def url(self, name):
         return urljoin(settings.MEDIA_URL, name)
+
+    def exists(self, name):
+        try:
+            self.connection.meta.client.head_object(Bucket=self.bucket_name, Key=self._normalize_name(self._clean_name(name)))
+            return True
+        except:
+            return False
+
+    def size(self, name):
+        try:
+            obj = self.connection.meta.client.head_object(Bucket=self.bucket_name, Key=self._normalize_name(self._clean_name(name)))
+            return int(obj['ContentLength'])
+        except:
+            raise SuspiciousOperation("Size not available.")
+
+    def modified_time(self, name):
+        try:
+            obj = self.connection.meta.client.head_object(Bucket=self.bucket_name, Key=self._normalize_name(self._clean_name(name)))
+            return obj['LastModified']
+        except:
+            raise SuspiciousOperation("Modification time not available.")
 
     def get_accessed_time(self, name):
         return None
