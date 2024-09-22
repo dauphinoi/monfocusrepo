bbeef4e88a2138c5d0c7bf7e3644be6f7b61c9e4
dauphinoi <ibrahima.diao@dauphine.eu>
2024-09-22 15:54:45 +0200

    mise a jour espaceteach 2

--- monFocusprof/urls.py
+++ monFocusprof/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('institution-contact/', views.institution_contact_view, name='institution_contact'),
-    # path('product/', views.product_view, name='product'),
+    path('product/', views.product_view, name='product'),
 ]
